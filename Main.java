d6f8d49f9b754a3d183f21eeeddf3a09e7ea887a
bikramia <bikram.rhce@gmail.com>
2025-01-30 12:52:13 -0500

    Initial commit -Arrays-Working with arrays in Java-best practice

--- Main.java
+++ Main.java
@@ -2,12 +2,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = new int[5];
-        numbers[0] = 10;
-        numbers[1] = 20;
-        numbers[2] = 30;
-        numbers[3] = 40;
-        numbers[4] = 50;
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println(numbers.length);
         System.out.println(Arrays.toString(numbers));
     }
 }
