58cfa0f42f2029f3661a776919f5805d1e1c55b7
bikramia <bikram.rhce@gmail.com>
2025-01-30 12:41:15 -0500

    Initial commit -Arrays-Working with arrays in Java

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
+        numbers[4] = 50;
+        System.out.println(Arrays.toString(numbers));
+    }
+}
